3f1285be703013e0bf890e764460b45d546eadce
frd nrdb <froden@gmail.com>
2026-02-13 15:30:11 +0100

    bump

--- src/fvn-ui/index.js
+++ src/fvn-ui/index.js
@@ -39,13 +39,16 @@ export {
 
 // Namespaced export for cleaner DX: ui.button(), ui.switch(), etc.
 import * as components from './components/index.js'
-import { layout, dom, colors } from './dom.js'
+import { layout, dom, el, row, col, colors } from './dom.js'
 export const ui = {
   ...components,
   select: components.selectComponent,
   switch: components.switchComponent,
   layout,
   dom,
+  el,
+  row,
+  col,
   colors
 };
 
